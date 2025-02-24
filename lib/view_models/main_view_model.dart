cf756b7eb6b260170bc30d5ea4c8be99374405e7
mdaniel3 <mdaniel3@mail.uni-paderborn.de>
2025-02-24 22:49:04 +0100

    added loading indicator when picking files

--- lib/view_models/main_view_model.dart
+++ lib/view_models/main_view_model.dart
@@ -12,6 +12,7 @@ class MainViewModel extends ChangeNotifier {
   var logger = Logger();
   late StreamSubscription _intentSub;
   final _sharedFiles = <SharedMediaFile>[];
+  final ValueNotifier<bool> isLoading = ValueNotifier<bool>(false);
 
   /// Dispose the sharing intent
   void disposeSharingIntent() {
@@ -48,6 +49,7 @@ class MainViewModel extends ChangeNotifier {
     logger.i(_sharedFiles.map((f) => f.toMap()).toString());
     notifyListeners();
 
+    isLoading.value = true;
     for (var file in _sharedFiles) {
       if (file.path.endsWith('.gpx') ||
           file.path.endsWith('.json') ||
@@ -63,6 +65,7 @@ class MainViewModel extends ChangeNotifier {
         );
       }
     }
+    isLoading.value = false;
   }
 
   /// Login to wanderer
--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -21,6 +21,7 @@ class Homepage extends StatelessWidget {
     if (result != null) {
       // ignore: use_build_context_synchronously
       final viewModel = context.read<MainViewModel>();
+      viewModel.isLoading.value = true;
       for (var file in result.files) {
         if (file.path != null) {
           await viewModel.uploadGpx(file.path!);
@@ -32,77 +33,112 @@ class Homepage extends StatelessWidget {
           );
         }
       }
+      viewModel.isLoading.value = false;
     }
   }
 
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<HomePageViewModel>();
+    final mainViewModel = context.watch<MainViewModel>();
+
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'Share to Wanderer',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Row(
-              children: [
-                ValueListenableBuilder<bool>(
-                  valueListenable: viewModel.isDarkTheme,
-                  builder: (context, isDarkTheme, child) {
-                    return IconButton(
-                      onPressed: () async {
-                        await viewModel.setTheme(!isDarkTheme);
-                      },
-                      icon: Icon(
-                        isDarkTheme ? Icons.dark_mode : Icons.light_mode,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: ValueListenableBuilder<bool>(
+          valueListenable: mainViewModel.isLoading,
+          builder: (context, isLoading, child) {
+            return AppBar(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Share to Wanderer',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    children: [
+                      ValueListenableBuilder<bool>(
+                        valueListenable: viewModel.isDarkTheme,
+                        builder: (context, isDarkTheme, child) {
+                          return IconButton(
+                            onPressed: () async {
+                              await viewModel.setTheme(!isDarkTheme);
+                            },
+                            icon: Icon(
+                              isDarkTheme ? Icons.dark_mode : Icons.light_mode,
+                            ),
+                            tooltip: "Toggle theme",
+                          );
+                        },
                       ),
-                      tooltip: "Toggle theme",
-                    );
-                  },
+                      IconButton(
+                        onPressed: () async {
+                          final Uri url = Uri.parse(
+                            'https://github.com/doen1el/share-to-wanderer/issues/new',
+                          );
+                          if (!await launchUrl(url)) {
+                            throw Exception('Could not launch $url');
+                          }
+                        },
+                        icon: Icon(Icons.bug_report_rounded),
+                        tooltip: "Report a bug",
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              backgroundColor: isLoading ? Colors.black54 : null,
+            );
+          },
+        ),
+      ),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 10),
+                TextDisplay(
+                  text:
+                      "Enter your wanderer credentials below. Then you can share your GPX files to this app and they will be uploaded to your wanderer instance.",
                 ),
-                IconButton(
-                  onPressed: () async {
-                    final Uri url = Uri.parse(
-                      'https://github.com/doen1el/share-to-wanderer/issues/new',
-                    );
-                    if (!await launchUrl(url)) {
-                      throw Exception('Could not launch $url');
-                    }
-                  },
-                  icon: Icon(Icons.bug_report_rounded),
-                  tooltip: "Report a bug",
+                SizedBox(height: 30),
+                CredentialsForm(),
+                SizedBox(height: 30),
+
+                TextDisplay(
+                  text:
+                      "You can also upload GPX files manually by clicking the button below.",
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+          ValueListenableBuilder<bool>(
+            valueListenable: mainViewModel.isLoading,
+            builder: (context, isLoading, child) {
+              if (isLoading) {
+                return Container(
+                  color: Colors.black54,
+                  child: Center(child: CircularProgressIndicator()),
+                );
+              } else {
+                return SizedBox.shrink();
+              }
+            },
+          ),
+        ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 10),
-            TextDisplay(
-              text:
-                  "Enter your wanderer credentials below. Then you can share your GPX files to this app and they will be uploaded to your wanderer instance.",
-            ),
-            SizedBox(height: 30),
-            CredentialsForm(),
-            SizedBox(height: 30),
-
-            TextDisplay(
-              text:
-                  "You can also upload GPX files manually by clicking the button below.",
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _pickAndUploadFiles(context),
-        tooltip: "Upload GPX files",
-        child: Icon(Icons.upload_rounded),
+      floatingActionButton: ValueListenableBuilder<bool>(
+        valueListenable: mainViewModel.isLoading,
+        builder: (context, isLoading, child) {
+          return FloatingActionButton(
+            onPressed: isLoading ? null : () => _pickAndUploadFiles(context),
+            tooltip: "Upload GPX files",
+            backgroundColor: isLoading ? Colors.grey : null,
+            child: Icon(Icons.upload_rounded),
+          );
+        },
       ),
     );
   }
